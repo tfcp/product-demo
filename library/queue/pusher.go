1dd18c1478cc536ed782096ec42266b775a534d7
tmaker <you@example.com>
2022-04-17 16:15:12 +0800

    kafka

--- library/queue/pusher.go
+++ library/queue/pusher.go
@@ -4,19 +4,16 @@ import "gf/library/queue/engine/kafka"
 
 type Pusher interface {
 	// producer
-	Push() error
-
+	Push(data []byte, topic string, blockKey ...string) error
 }
 
-
-func NewPusher(pusherTypes ...string) Pusher{
+func NewPusher(pusherTypes ...string) Pusher {
 	var pusher Pusher
-	if len(pusherTypes) == 0{
+	if len(pusherTypes) == 0 {
 		pusherTypes = []string{"kafka"}
 	}
 	switch pusherTypes[0] {
 	case "kafka":
-		kafkaAddr := []string{}
 		pusher = kafka.NewKafkaPusher(kafkaAddr)
 		break
 	}
