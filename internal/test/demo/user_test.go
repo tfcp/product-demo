84f19e785a70083af6d20656937c371c93964e13
tmaker <you@example.com>
2022-05-02 22:00:28 +0800

    debug

--- internal/test/demo/user_test.go
+++ internal/test/demo/user_test.go
@@ -1,1 +1,30 @@
 package demo
+
+import (
+	"github.com/gogf/gf/test/gtest"
+	"testing"
+	"tfpro/internal/test"
+)
+
+func init() {
+	test.SetupServer()
+}
+
+func Test_UserService(t *testing.T) {
+	// info
+	gtest.C(t, func(t *gtest.T) {
+		whereCondition := map[string]interface{}{
+			"name": "tom",
+		}
+		_, err := svc.One(whereCondition)
+		t.Assert(err, nil)
+	})
+	// list
+	gtest.C(t, func(t *gtest.T) {
+		whereCondition := map[string]interface{}{
+			"name": "",
+		}
+		_, err := svc.List(whereCondition)
+		t.Assert(err, nil)
+	})
+}
--- internal/test/test.go
+++ internal/test/test.go
@@ -1,10 +1,17 @@
 package test
 
 import (
-	"tfpro/internal/model"
-	"tfpro/library/gredis"
-	"tfpro/library/log"
 	"github.com/gogf/gf/frame/g"
+	v1 "tfpro/app/grpc/v1"
+	"tfpro/internal/model"
+	"tfpro/internal/service/demo"
+	"tfpro/library/gredis"
+	"tfpro/library/grpc"
+	"tfpro/library/log"
+)
+
+var (
+	TestgRPCServer = grpc.Default()
 )
 
 func SetupServer() {
@@ -17,4 +24,10 @@ func SetupServer() {
 		log.Logger.Fatalf("db init error:%v", err)
 		return
 	}
+	// 应用配置初始化
+	if err := TestgRPCServer.InitServer(); err != nil {
+		log.Logger.Fatal(err)
+	}
+	// 注册grpc server
+	v1.RegisterUserServer(TestgRPCServer.Server, new(demo.RpcServer))
 }
