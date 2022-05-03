caa2f0a4f00d5986b7c9ea7e23b7eacf425e2e6b
tmaker <you@example.com>
2022-05-03 21:42:58 +0800

    debug

--- internal/service/demo/user.go
+++ internal/service/demo/user.go
@@ -5,14 +5,23 @@ import (
 	v1 "tfpro/app/grpc/v1"
 )
 
-func (this *RpcServer) Info(context.Context, *v1.InfoReq) (*v1.InfoRes, error) {
+func (this *RpcServer) Info(ctx context.Context, req *v1.InfoReq) (*v1.InfoRes, error) {
 	res := v1.InfoRes{
-		Name: "test",
-		Age:  24,
+		Name:   "test-info",
+		Age:    1,
+		UserId: req.UserId,
 	}
 	return &res, nil
 }
 
-func (this *RpcServer) List(context.Context, *v1.ListReq) (*v1.ListRes, error) {
-	return &v1.ListRes{}, nil
+func (this *RpcServer) List(ctx context.Context, req *v1.ListReq) (*v1.ListRes, error) {
+	resList := new(v1.ListRes)
+	resList.List = make([]*v1.InfoRes, 0)
+	info := v1.InfoRes{
+		Name:   "test-list",
+		Age:    2,
+		UserId: req.UserId,
+	}
+	resList.List = append(resList.List,&info)
+	return resList, nil
 }
