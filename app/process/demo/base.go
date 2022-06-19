5e32f19957049f396a5586df912550d5abc70753
sujizhao <sujizhao@tencent.com>
2022-06-19 16:54:58 +0800

    process

--- app/process/demo/base.go
+++ app/process/demo/base.go
@@ -1,7 +1,13 @@
 package demo
 
-func ProcessDemo() {
+import "time"
+
+func ProcessDemo() [][]string{
 	// demo
 	go HelloProcess()
 	go TestProcess()
+	return [][]string{
+		// cluster
+		{"demo进程","sujizhao", time.Now().Format("2006-01-02 15:04:05"), "备注"},
+	}
 }
--- app/process/process.go
+++ app/process/process.go
@@ -1,8 +1,20 @@
 package process
 
-import "tfpro/app/process/demo"
+import (
+	"tfpro/app/process/demo"
+	"tfpro/library/utils"
+)
+
+var (
+	processStdoutTitle    = []string{"进程名称", "负责人", "创建时间", "备注"}
+	processStdoutContents = [][]string{}
+)
 
 // consumer process
 func Process() {
-	demo.ProcessDemo()
+	demoStdout := demo.ProcessDemo()
+	processStdoutContents = append(processStdoutContents, demoStdout...)
+
+	// stdout cron list
+	utils.TableStdout(processStdoutTitle, processStdoutContents)
 }
